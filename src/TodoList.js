61cf2b776f522bbea8a812977addc99028314140
khanhafe <khanhafe@gmail.com>
2018-09-14 14:49:44 +0530

    hhh

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,7 +3,9 @@ import React from "react";
 class TodoList extends React.Component {
   constructor(props) {
     super(props);
-
+    this.state = {
+      items: []
+    };
     this.addItem = this.addItem.bind(this);
   }
 
