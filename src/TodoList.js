f0642f9ac4170311120e9d163ed3844c82a61652
khanhafe <khanhafe@gmail.com>
2018-09-14 12:29:12 +0530

    todoapp

---
+++ src/TodoList.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+class TodoList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.addItem = this.addItem.bind(this);
+  }
+
+  addItem(e) {}
+
+  render() {
+    return (
+      <div className="todoListMain">
+        <div className="header">
+          <form onSubmit={this.addItem}>
+            <input placeholder="enter task" />
+            <button type="submit">add</button>
+          </form>
+        </div>
+      </div>
+    );
+  }
+}
+export default TodoList;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import TodoList from "./TodoList";
 
 import "./styles.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <TodoList />
     </div>
   );
 }
